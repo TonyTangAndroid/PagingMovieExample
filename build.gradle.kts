15b974bc86ddd933c407131e83a4a08a7b6c5066
Tony Tang <tonytang@uber.com>
2024-01-11 23:00:55 -0800

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
-    id("com.android.application") version Versions.gradle  apply false
-    id("com.android.library") version Versions.gradle apply false
+    id("com.android.application") version Versions.gradleAndroid  apply false
+    id("com.android.library") version Versions.gradleAndroid apply false
     id("org.jetbrains.kotlin.android") version Versions.kotlin apply false
     id("com.google.dagger.hilt.android") version Versions.hilt apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
